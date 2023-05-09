bc6233de9ee81726b86fdd5eca6da0e2373ca468
HeremansGeert <herge@sintcordula.be>
2023-05-09 08:47:01 +0200

    test

--- routes/auth.php
+++ routes/auth.php
@@ -10,12 +10,22 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+    use Carbon\Carbon;
 
 Route::middleware('guest')->group(function () {
+
+if (Carbon::now() > Carbon::parse('2023-05-09 10:30:00')) {
+    Route::get('/register', function () {
+        abort(404);
+    });
+} else {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
+        Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+}
+
+
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
